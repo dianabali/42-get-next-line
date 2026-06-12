8ff4f184b8b1f5854c8a02d55f9ffe37c4866dc7
Diana <dianabali64@gmail.com>
2026-06-12 15:31:43 +0200

    get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+// Main function
+char	*get_next_line(int fd);
+
+// Utils
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
